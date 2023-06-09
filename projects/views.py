8f19c59b50b2fb8c79f48aa9531ac47f2513f48a
Sung Hong <whsghd3430@gmail.com>
2023-06-09 01:22:45 -0400

    Feature 7 complete

--- projects/views.py
+++ projects/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from projects.models import Project
+from django.contrib.auth.decorators import login_required
 
 
+@login_required
 def list_projects(request):
     projects = Project.objects.all()
     context = {
--- tracker/urls.py
+++ tracker/urls.py
@@ -27,4 +27,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("projects/", include("projects.urls")),
     path("", redirect_to_list_projects, name="home"),
+    path("accounts/", include("accounts.urls")),
 ]
